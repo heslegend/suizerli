9c93e4c2fca481d1ba30f9ac2ea4883f7048f681
Benjamin Stoeberl <42295655+heslegend@users.noreply.github.com>
2020-01-11 16:55:01 +0100

    Add CustomScrollView with Sliver + TabBar Navigation

--- suizerli/lib/UI/home/feed/feed_item.dart
+++ suizerli/lib/UI/home/feed/feed_item.dart
@@ -11,29 +11,30 @@ class _FeedItemState extends State<FeedItem> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 150,
       child: Card(
         semanticContainer: true,
         clipBehavior: Clip.antiAliasWithSaveLayer,
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
         elevation: 5,
-        margin: EdgeInsets.all(5.0),
+        margin: EdgeInsets.all(10.0),
         child: Column(
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             Container(
-                height: 200,
+                height: 100,
                 width: double.infinity,
-                child: Image.asset(
-                  'images/woods.jpg',
-                  fit: BoxFit.fill,
+                child: Image.network(
+                  'https://picsum.photos/300?grayscale&blur',
+                  fit: BoxFit.cover,
                 )),
             Text(
                 "Why maldon salt is the best salt!",
             style: TextStyle(
-              fontWeight: FontWeight.bold,
+              fontFamily: 'SegoeUi',
               fontSize:20.0,
+              color: Colors.teal
             ),),
           ],
         ),
--- suizerli/lib/UI/home/my_feed.dart
+++ suizerli/lib/UI/home/my_feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:suizerli/UI/home/feed/feed_item.dart';
 
 class MyFeed extends StatefulWidget {
@@ -7,7 +8,20 @@ class MyFeed extends StatefulWidget {
 }
 
 class _MyFeedState extends State<MyFeed> {
-  static List<Widget> _recommendationList = [FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem(), FeedItem()];
+  static List<Widget> _recommendationList = [
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem(),
+    FeedItem()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +29,97 @@ class _MyFeedState extends State<MyFeed> {
   }
 
   Widget _body() {
-    return ListView(
-      children:  _recommendationList,
+    return DefaultTabController(
+        length: 12,
+        child: Container(
+            child: Container(height: 100, child: sliverScrollView())));
+  }
+
+  Widget navigationTextElement(String text) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+      child: Text(
+        text,
+        style:
+            TextStyle(color: Colors.teal, fontFamily: 'SegoeUi', fontSize: 30),
+      ),
     );
   }
 
+  Widget displayNewsFeed() {
+    return Container(
+        height: 900, child: ListView(children: _recommendationList));
+  }
+
+  Widget sliverScrollView() {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          floating: true,
+          expandedHeight: 150.0,
+          pinned: true,
+          title: Text("SUIZERLI"),
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            background: Image.asset('images/sitting.jpg',fit: BoxFit.cover),
+          ),
+          bottom: TabBar(
+            isScrollable: true,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white.withOpacity(0.3),
+            tabs: <Widget>[
+              Tab(text: "Art"),
+              Tab(text: "Food"),
+              Tab(text: "Music"),
+              Tab(text: "Bars"),
+              Tab(text: "Movies"),
+              Tab(text: "Books"),
+              Tab(text: "Tech"),
+              Tab(text: "Travel"),
+              Tab(text: "Outdoor"),
+              Tab(text: "Sports"),
+              Tab(text: "Comedy"),
+              Tab(text: "Salt"),
+            ],
+          ),
+        ),
+        SliverFixedExtentList(
+          itemExtent: 150.0,
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return Container(
+                alignment: Alignment.center,
+                child: FeedItem(),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final _tabBar;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
 }

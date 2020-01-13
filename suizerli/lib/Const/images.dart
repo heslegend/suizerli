8d6a4aef501f1cf2042472f1b171ec19a9167fe3
Benjamin Stoeberl <42295655+heslegend@users.noreply.github.com>
2020-01-13 18:44:28 +0100

    Refactor feed

---
+++ suizerli/lib/Const/images.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Image navArt = Image.asset('images/nav/nav_art.jpg', fit: BoxFit.cover);
+Image navSports = Image.asset('images/nav/nav_sport_w.jpg', fit: BoxFit.cover);
+Image navOutdoor = Image.asset('images/nav/nav_outdoor.jpg', fit: BoxFit.cover);
+Image navTech = Image.asset('images/nav/nav_tech.jpg', fit: BoxFit.cover);
+Image navBar = Image.asset('images/nav/nav_bar.jpg', fit: BoxFit.cover);
+Image navFood = Image.asset('images/nav/nav_food.jpg', fit: BoxFit.cover);
+Image navBook = Image.asset('images/nav/nav_books.jpg', fit: BoxFit.cover);
+Image navCinema = Image.asset('images/nav/nav_cinema.jpg', fit: BoxFit.cover);
+Image navTravel = Image.asset('images/nav/nav_travel.jpg', fit: BoxFit.cover);
+Image navMusic = Image.asset('images/nav/nav_music.jpg', fit: BoxFit.cover);
--- suizerli/lib/UI/home/my_feed.dart
+++ suizerli/lib/UI/home/my_feed.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:suizerli/Const/images.dart';
 import 'package:suizerli/UI/home/feed/feed_item.dart';
 
 class MyFeed extends StatefulWidget {
@@ -8,19 +9,18 @@ class MyFeed extends StatefulWidget {
 }
 
 class _MyFeedState extends State<MyFeed> {
-  static Image navArt = Image.asset('images/nav/nav_art.jpg', fit: BoxFit.cover);
-  static Image navSports = Image.asset('images/nav/nav_sport_w.jpg', fit: BoxFit.cover);
-  static Image navOutdoor = Image.asset('images/nav/nav_outdoor.jpg', fit: BoxFit.cover);
-  static Image navTech = Image.asset('images/nav/nav_tech.jpg', fit: BoxFit.cover);
-  static Image navBar = Image.asset('images/nav/nav_bar.jpg', fit: BoxFit.cover);
-  static Image navFood = Image.asset('images/nav/nav_food.jpg', fit: BoxFit.cover);
-  static Image navBook = Image.asset('images/nav/nav_books.jpg', fit: BoxFit.cover);
-  static Image navCinema = Image.asset('images/nav/nav_cinema.jpg', fit: BoxFit.cover);
-  static Image navTravel = Image.asset('images/nav/nav_travel.jpg', fit: BoxFit.cover);
-  static Image navMusic = Image.asset('images/nav/nav_music.jpg', fit: BoxFit.cover);
 
-  static List<Image> _categoryImages = [
-   navArt, navSports, navOutdoor, navTech, navBar, navFood, navBook, navCinema, navMusic, navTravel
+  List<Image> _categoryImages = [
+    navArt,
+    navSports,
+    navOutdoor,
+    navTech,
+    navBar,
+    navFood,
+    navBook,
+    navCinema,
+    navMusic,
+    navTravel
   ];
 
   static List<Tab> _categoryTabs = [
@@ -32,7 +32,7 @@ class _MyFeedState extends State<MyFeed> {
     Tab(text: "Food"),
     Tab(text: "Books"),
     Tab(text: "Movies"),
-    Tab(text: "Comedy"),
+    Tab(text: "Music"),
     Tab(text: "Travel"),
   ];
 
@@ -44,47 +44,60 @@ class _MyFeedState extends State<MyFeed> {
   }
 
   Widget _body() {
-    return DefaultTabController(length: _categoryTabs.length, child: sliverScrollView());
+    return DefaultTabController(
+        length: _categoryTabs.length, child: sliverScrollView());
   }
 
   Widget sliverScrollView() {
     return CustomScrollView(
       slivers: <Widget>[
-        SliverAppBar(
-          floating: true,
-          expandedHeight: 200.0,
-          pinned: true,
-          title: Text("SUIZERLI."),
-          flexibleSpace: FlexibleSpaceBar(
-            centerTitle: true,
-            background: _categoryImages[currentIndex],
-          ),
-          bottom: TabBar(
-            isScrollable: true,
-            labelColor: Colors.white,
-            unselectedLabelColor: Colors.white.withOpacity(0.5),
-            tabs: _categoryTabs,
-            onTap: (int index) => _setCategoryImage(index),
-          ),
-        ),
-        SliverFixedExtentList(
-          itemExtent: 275.0,
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return Container(
-                alignment: Alignment.center,
-                child: FeedItem(),
-              );
-            },
-          ),
-        ),
+        categoryNavigationHeader(),
+        recommendationList(),
       ],
+    );
+  }
+
+
+  Widget categoryNavigationHeader(){
+    return SliverAppBar(
+      floating: true,
+      expandedHeight: 200.0,
+      pinned: true,
+      title: Text("SUIZERLI."),
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        background: _categoryImages[currentIndex],
+      ),
+      bottom: categoryNavigationTabBar(),
+    );
+  }
+
+  Widget categoryNavigationTabBar() {
+    return TabBar(
+      isScrollable: true,
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.white.withOpacity(0.5),
+      tabs: _categoryTabs,
+      onTap: (int index) => _setCategoryImage(index),
+    );
+  }
+
+  Widget recommendationList() {
+    return SliverFixedExtentList(
+      itemExtent: 275.0,
+      delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+          return Container(
+            alignment: Alignment.center,
+            child: FeedItem(),
+          );
+        },
+      ),
     );
   }
 
   void _setCategoryImage(int index) {
     currentIndex = index;
-    setState(() {
-    });
+    setState(() {});
   }
 }
